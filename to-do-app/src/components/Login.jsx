fb0a2fe9d6ba4a1c35c0de0d267e8f5919605224
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-06-17 13:49:32 +0200

    finish the login form

--- to-do-app/src/components/Login.jsx
+++ to-do-app/src/components/Login.jsx
@@ -11,12 +11,12 @@ const Login = () => {
 
     return (
         <div className="login-page">
-            
+
             <div className="login-form">
                 <h2>Login</h2>
                 <form onSubmit={handleData} action='post'>
-                <div className="form-items">
-                    
+                    <div className="form-items">
+
                         <div className="form-item">
                             <label htmlFor="email">Email</label>
                             <input
@@ -28,22 +28,22 @@ const Login = () => {
                                 required
                             />
                         </div>
-                    
+
                         <div className="form-item">
                             <label htmlFor="password">Password</label>
                             <input
                                 className='input-box'
-                                type="text"
+                                type="password"
                                 name="password"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
                                 required
                             />
                         </div>
-                        <div className="class-item submit-buttom">
-                            <button type="submit">Submit</button>
+                        <div className="class-item">
+                            <button type="submit" className="submit-button">Submit</button>
                         </div>
-                </div>
+                    </div>
                 </form>
             </div>
         </div>
