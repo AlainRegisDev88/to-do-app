9551406c18bcebc0d48d55e760df142ecda72cbf
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-02 11:43:41 +0200

    add login and signup function in authservice file

---
+++ to-do-app/src/services/authService.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+const API_URL = '/api/auth'
+
+const authService = {
+
+    //signup
+    signup: async (name, email, password) => {
+        try {
+            const response = await axios.post(`${API_URL}/signup`, {
+                name,
+                email,
+                password
+            })
+
+            if (response.data.token) {
+                localStorage.setItem('token', response.data.token)
+            }
+            return response.data
+        } catch (error) {
+            throw error.response?.data?.message || 'Signup Failed'
+        }
+
+    },
+    
+    // Login
+    login: async(email, password) => {
+        try{
+            const response = await axios.post(`${API_URL}/login`, {
+                email,
+                password
+            })
+
+            if (response.data.token){
+                localStorage.setItem('token', response.data.token)
+            }
+
+            return response.data
+        }catch(error){
+            throw error.response?.data?.message || "Login failed"
+        }
+    },
+
+    //logout
+    logout: () => {
+
+    }
+}
