f9bc1941ce981b14bf33ba2c36a2eb28b79af982
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-04 12:17:14 +0200

    upodate profile service path

--- to-do-app/src/services/profileServices.js
+++ to-do-app/src/services/profileServices.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 import api from "./api";
 
-const API_URL = '/api'
-
 const profileServices = {
 
     getPersonalData: async () => {
         try {
-            const response = await api.get(`${API_URL}/profile`)
+            const response = await api.get(`/profile`)
             return response.user
         } catch (error) {
             throw error.response?.data?.message || "Failed to get the info"
