aeb7e1aa3cf1d6481834214ea98c2348104b2e31
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-09 21:09:17 +0200

    fix: the spacing between the header elements

--- to-do-app/src/components/Header.jsx
+++ to-do-app/src/components/Header.jsx
@@ -5,14 +5,22 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
     return (
         <section className="header">
-            <div className="logo-section"><div className="tick-icon"><FontAwesomeIcon icon={faCheck} style={{ color: "#0C447C" }} /></div><div className="logo-text">tasklist</div></div>
-
-            <div className="search-section">
-                <input type="text" name="search" id="search-box" placeholder="Search tasks" className="search"/>
+            <div className="logo-section">
+                <div className="tick-icon">
+                    <FontAwesomeIcon icon={faCheck} style={{ color: "#0C447C" }} />
+                </div>
+                <div className="logo-text">
+                    tasklist
+                </div>
             </div>
 
-            <div className="nav">
-                <Link to='/'>Home</Link>
+            <div className="search-section">
+                <input type="text" name="search" id="search-box" placeholder="Search tasks" className="search" />
+            </div>
+
+
+            <div className="right-nav-bar">
+                <Link className="add-task-button">+ Add Task</Link>
                 <Link to='/tasks'>Tasks</Link>
             </div>
         </section>
