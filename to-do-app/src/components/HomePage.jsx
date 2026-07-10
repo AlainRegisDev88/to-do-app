d02402b26c5a153968f3f0694416e5a1815767dd
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-10 20:02:27 +0200

    add task status

--- to-do-app/src/components/HomePage.jsx
+++ to-do-app/src/components/HomePage.jsx
@@ -48,11 +48,18 @@ const HomePage = () => {
                         </div>
 
                         <section className="tasks-section">
-                            <div className="task-card">
+                            <div className="task-card active">
+                                <div className="checkbox"></div>
+                                <div className="task-title"></div>
+                                <div className="priority-tag priority-high">High</div>
+                            </div>
+
+                            <div className="task-card done">
                                 <div className="checkbox checked"></div>
                                 <div className="task-title task-active"></div>
                                 <div className="priority-tag priority-high">High</div>
                             </div>
+
                         </section>
 
                     </div>
