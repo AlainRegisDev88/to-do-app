34c47b3b493f45ecb5c4a9bbd06ecab1725078a5
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-10 12:35:16 +0200

    add active class to the nav-items

--- to-do-app/src/components/Sidebar/Sidebar.jsx
+++ to-do-app/src/components/Sidebar/Sidebar.jsx
@@ -3,15 +3,16 @@ import { NavLink } from "react-router-dom";
 import { faFolder, faSun } from "@fortawesome/free-regular-svg-icons";
 import { faCalendar } from "@fortawesome/free-regular-svg-icons";
 import { faCheck, faGear } from "@fortawesome/free-solid-svg-icons";
+import './Sidebar.css'
 
 const Sidebar = () => {
     return ( 
         <section className="sidebar">
-            <NavLink className='nav-item' to="/dashboard"><FontAwesomeIcon icon={faSun} /> Today</NavLink>
-            <NavLink className='nav-item' to="/upcoming-tasks"><FontAwesomeIcon icon={faCalendar} /> Today</NavLink>
-            <NavLink className='nav-item' to="/projects"><FontAwesomeIcon icon={faFolder} /> Today</NavLink>
-            <NavLink className='nav-item' to="/completed-tasks"><FontAwesomeIcon icon={faCheck} /> Today</NavLink>
-            <NavLink className='nav-item' to="/settings"><FontAwesomeIcon icon={faGear} /> Today</NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'nav-item active-link' : 'nav-item'} to="/dashboard"><FontAwesomeIcon icon={faSun} /><p className="navlink-text">Today</p></NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'nav-item active-link' : 'nav-item'} to="/upcoming-tasks"><FontAwesomeIcon icon={faCalendar} /><p className="navlink-text">Upcoming</p></NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'nav-item active-link' : 'nav-item'} to="/projects"><FontAwesomeIcon icon={faFolder} /><p className="navlink-text">Projects</p></NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'nav-item active-link' : 'nav-item'} to="/completed-tasks"><FontAwesomeIcon icon={faCheck} /><p className="navlink-text">completed</p></NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'nav-item active-link' : 'nav-item'} to="/settings"><FontAwesomeIcon icon={faGear} /><p className="navlink-text">Settings</p></NavLink>
         </section>
      );
 }
