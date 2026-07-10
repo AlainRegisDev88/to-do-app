a76c30ca61822a27ec943c7d312f3bacd871c131
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-10 12:13:34 +0200

    add nav-item classes

--- to-do-app/src/components/Sidebar/Sidebar.jsx
+++ to-do-app/src/components/Sidebar/Sidebar.jsx
@@ -7,11 +7,11 @@ import { faCheck, faGear } from "@fortawesome/free-solid-svg-icons";
 const Sidebar = () => {
     return ( 
         <section className="sidebar">
-            <NavLink to="/dashboard"><FontAwesomeIcon icon={faSun} /> Today</NavLink>
-            <NavLink to="/upcoming-tasks"><FontAwesomeIcon icon={faCalendar} /> Today</NavLink>
-            <NavLink to="/projects"><FontAwesomeIcon icon={faFolder} /> Today</NavLink>
-            <NavLink to="/completed-tasks"><FontAwesomeIcon icon={faCheck} /> Today</NavLink>
-            <NavLink to="/settings"><FontAwesomeIcon icon={faGear} /> Today</NavLink>
+            <NavLink className='nav-item' to="/dashboard"><FontAwesomeIcon icon={faSun} /> Today</NavLink>
+            <NavLink className='nav-item' to="/upcoming-tasks"><FontAwesomeIcon icon={faCalendar} /> Today</NavLink>
+            <NavLink className='nav-item' to="/projects"><FontAwesomeIcon icon={faFolder} /> Today</NavLink>
+            <NavLink className='nav-item' to="/completed-tasks"><FontAwesomeIcon icon={faCheck} /> Today</NavLink>
+            <NavLink className='nav-item' to="/settings"><FontAwesomeIcon icon={faGear} /> Today</NavLink>
         </section>
      );
 }
