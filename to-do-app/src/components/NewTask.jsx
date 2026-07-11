1a722cf9e40216437465cc8d11dffbd4c8f3a9ba
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-11 19:41:45 +0200

    add the close button in the top corner

--- to-do-app/src/components/NewTask.jsx
+++ to-do-app/src/components/NewTask.jsx
@@ -1,9 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './NewTask.css'
+import { faClose, faCross, faX } from '@fortawesome/free-solid-svg-icons';
 
 const NewTask = () => {
     return ( 
         <section className="new-task-page">
             <div className="new-task-card">
+                <div className="close-icon"><FontAwesomeIcon icon={faClose} /></div>
 
             </div>
         </section>
