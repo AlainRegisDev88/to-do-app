ff5c61b6afb337b05f6135f2ba9824846ddd0991
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-11 19:23:34 +0200

    add the new task component

---
+++ to-do-app/src/components/NewTask.jsx
@@ -0,0 +1,11 @@
+const NewTask = () => {
+    return ( 
+        <section className="new-task-page">
+            <div className="new-task-form">
+
+            </div>
+        </section>
+     );
+}
+ 
+export default NewTask;
