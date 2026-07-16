f6cfd1561b48c98bf6cd05a192793a0269d54696
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-16 20:29:41 +0200

    link a homelayout outlet in app component

--- to-do-app/src/components/HomeLayout.jsx
+++ to-do-app/src/components/HomeLayout.jsx
@@ -2,12 +2,14 @@ import { Routes, Route, Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer/Footer";
 import './HomeLayout.css'
+import Sidebar from "./Sidebar/Sidebar";
 
 const HomeLayout = () => {
     return ( 
         <>
             <Header />
             <div className="main">
+                <Sidebar />
                 <Outlet />
             </div>
             <Footer />
