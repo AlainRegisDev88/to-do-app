e8895241ab4e00ff2b7b5b0e5f3cad64bb47313b
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-16 21:27:09 +0200

    create a grid of the projects

--- to-do-app/src/components/ProjectsPage.jsx
+++ to-do-app/src/components/ProjectsPage.jsx
@@ -1,29 +1,60 @@
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faAdd, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './ProjectsPage.css'
 
 const ProjectsPage = () => {
-    return ( 
+    return (
         <section className="projects-page">
-            
-            <div className="projects-container">
-                <div className="projects-header projects-item">
+            <div className="projects-header">
                     <p>Projects</p>
+                </div>
+            <div className="projects-container">
+                <div className="project-card">
+                    <div className="project-header">
+                        <FontAwesomeIcon icon={faUser} />
+                        <p className="project-title">Personal</p>
+                    </div>
+                    <p className="open-tasks-count">
+                        6 open tasks
+                    </p>
                 </div>
 
                 <div className="project-card">
                     <div className="project-header">
                         <FontAwesomeIcon icon={faUser} />
-                        <p className="open-tasks-count">
-                            6 open tasks
-                        </p>
+                        <p className="project-title">Personal</p>
                     </div>
+                    <p className="open-tasks-count">
+                        6 open tasks
+                    </p>
                 </div>
-                <div className="project-card"></div>
-                <div className="project-card"></div>
-                <div className="project-card"></div>
-                <div className="project-card"></div>
+
+                <div className="project-card">
+                    <div className="project-header">
+                        <FontAwesomeIcon icon={faUser} />
+                        <p className="project-title">Personal</p>
+                    </div>
+                    <p className="open-tasks-count">
+                        6 open tasks
+                    </p>
+                </div>
+
+                <div className="project-card">
+                    <div className="project-header">
+                        <FontAwesomeIcon icon={faUser} />
+                        <p className="project-title">Personal</p>
+                    </div>
+                    <p className="open-tasks-count">
+                        6 open tasks
+                    </p>
+                </div>
+
+                <div className="project-card add-new-task-card">
+                    <FontAwesomeIcon icon={faAdd} />
+                    <p>Add new project</p>
+                </div>
             </div>
         </section>
-     );
+    );
 }
 export default ProjectsPage;
