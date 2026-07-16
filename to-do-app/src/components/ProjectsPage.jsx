e2887a7b18770185aed76880014b1a886f901924
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-16 20:07:28 +0200

    add one project card

--- to-do-app/src/components/ProjectsPage.jsx
+++ to-do-app/src/components/ProjectsPage.jsx
@@ -1,8 +1,26 @@
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const ProjectsPage = () => {
     return ( 
         <section className="projects-page">
             <div className="projects-container">
-                
+                <div className="projects-header projects-item">
+                    <p>Projects</p>
+                </div>
+
+                <div className="project-card">
+                    <div className="project-header">
+                        <FontAwesomeIcon icon={faUser} />
+                        <p className="open-tasks-count">
+                            6 open tasks
+                        </p>
+                    </div>
+                </div>
+                <div className="project-card"></div>
+                <div className="project-card"></div>
+                <div className="project-card"></div>
+                <div className="project-card"></div>
             </div>
         </section>
      );
