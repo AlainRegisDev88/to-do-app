117ea8febd8b00c50a35d833bc3bf14155aca2b4
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-16 12:55:34 +0200

    create html layout for create-new-task form

--- to-do-app/src/components/NewTask.jsx
+++ to-do-app/src/components/NewTask.jsx
@@ -3,11 +3,54 @@ import './NewTask.css'
 import { faClose} from '@fortawesome/free-solid-svg-icons';
 
 const NewTask = () => {
+
+    const addNewTask = () =>{
+
+    }
     return ( 
         <section className="new-task-page">
             <div className="new-task-card">
                 <div className="close-icon"><FontAwesomeIcon icon={faClose} /></div>
+                <form onSubmit={addNewTask} method="post">
 
+                    <div className="add-task-form-item">
+                        <label htmlFor="title">Title</label>
+                        <input className='tasks-input-box' type="text" name="title" placeholder='What needs to get done?'/>
+                    </div>
+
+                    <div className="add-task-form-item">
+                        <label htmlFor="description">Description</label>
+                        <textarea className="tasks-input-box" name="description" placeholder='Add more details(optional)'></textarea>
+                    </div>
+
+                    <div className="add-task-form-item">
+                        <div className="due-date-field">
+                            <label htmlFor="task-date">Due date</label>
+                            <input type="date" name="task-date" />
+                        </div>
+                        <div className="priority-field">
+                            <label htmlFor="task-priority">Priority</label>
+                            <select name="task-priority">
+                                <option value="high">High</option>
+                                <option value="medium">Medium</option>
+                                <option value="low">Low</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className="add-task-form-item">
+                        <label htmlFor="project-selection"> Project</label>
+                        <select name="project-selection">
+                            <option value="None">None</option>
+                            <option value="Project 1">Project 1</option>
+                        </select>
+                    </div>
+
+                    <div className="add-task-form-item">
+                        <div className="cancel-button">Cancel</div>
+                        <div className="create-task-button">Create task</div>
+                    </div>
+                </form>
             </div>
         </section>
      );
