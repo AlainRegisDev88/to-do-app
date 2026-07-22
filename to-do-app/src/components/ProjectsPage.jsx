be41695045bcb8f6a704732eba30d82e41f8b121
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-22 21:20:12 +0200

    create a map method

--- to-do-app/src/components/ProjectsPage.jsx
+++ to-do-app/src/components/ProjectsPage.jsx
@@ -2,25 +2,24 @@ import { faAdd, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import './ProjectsPage.css'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import projectsServices from "../services/projectsServices";
 
 
 
 const ProjectsPage = () => {
+    const[projects, setProjects] =  useState(null)
 
     useEffect(() => {
         const getProjects = async () => {
             const results = await projectsServices.fetchProjects()
             console.log(results)
-            return results.projects
+            setProjects(results.projects)
         }
 
-        const projects = getProjects();
-    }, [])
-
-
-
+        getProjects()
+        console.log(projects)
+    }, [projects])
 
     return (
         <section className="projects-page">
@@ -28,15 +27,21 @@ const ProjectsPage = () => {
                 <p>Projects</p>
             </div>
             <div className="projects-container">
-                <div className="project-card">
-                    <div className="project-header">
-                        <FontAwesomeIcon icon={faUser} style={{ color: "darkgreen" }} />
-                        <p className="project-title">Personal</p>
-                    </div>
-                    <p className="open-tasks-count">
-                        6 open tasks
-                    </p>
-                </div>
+
+                {projects.map((project) => {
+                    return (
+                        <div className="project-card">
+                            <div className="project-header">
+                                <FontAwesomeIcon icon={faUser} style={{ color: "darkgreen" }} />
+                                <p className="project-title">Personal</p>
+                            </div>
+                            <p className="open-tasks-count">
+                                6 open tasks
+                            </p>
+                        </div>
+
+                    )
+                })}
 
                 <div className="project-card">
                     <div className="project-header">
@@ -68,7 +73,7 @@ const ProjectsPage = () => {
                     </p>
                 </div>
 
-                <Link to="/new-project" className="project-card add-new-task-card">
+                <Link to="/" className="project-card add-new-task-card">
                     <FontAwesomeIcon icon={faAdd} />
                     <p>Add new project</p>
                 </Link>
