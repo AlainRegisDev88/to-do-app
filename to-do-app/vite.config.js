0f83ccac1a1e5f2ab876d9f47f5d543f28b2da5d
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-23 20:12:18 +0200

    fix: add server.watch configuration

--- to-do-app/vite.config.js
+++ to-do-app/vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     watch: {
+//       usePolling: true, // Forces Vite to check files manually for updates
+//     },
+//   },
+// })
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -9,6 +20,9 @@ export default defineConfig({
       '/api':{
         target: 'http://localhost:5000'
       }
+    },
+    watch: {
+      usePolling: true, // Forces Vite to check files manually for updates
     }
   }
 })
