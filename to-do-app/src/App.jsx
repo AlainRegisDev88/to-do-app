cbc4c00682ca326244140b33a63fc0777e8db2d6
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-23 22:06:44 +0200

    import new project

--- to-do-app/src/App.jsx
+++ to-do-app/src/App.jsx
@@ -13,6 +13,7 @@ import HomeLayout from './components/HomeLayout'
 import SettingsPage from './components/SettingsPage'
 import Empty from './components/Empty'
 import projectsServices from './services/projectsServices'
+import NewProject from './components/Pages/NewProject/NewProject'
 
 function App() {
   const [user, setUser] = useState([])
