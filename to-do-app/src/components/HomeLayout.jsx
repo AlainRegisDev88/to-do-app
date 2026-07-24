76210fc341525cf3ab5407f7cf79d8b8478ac209
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-24 22:43:19 +0200

    redirect to homepage when there is no token

--- to-do-app/src/components/HomeLayout.jsx
+++ to-do-app/src/components/HomeLayout.jsx
@@ -3,9 +3,18 @@ import Header from "./Header";
 import Footer from "./Footer/Footer";
 import './HomeLayout.css'
 import Sidebar from "./Sidebar/Sidebar";
+import { Navigate } from "react-router-dom";
 
-const HomeLayout = ({user}) => {
-    return ( 
+const HomeLayout = ({ user }) => {
+
+    const token = localStorage.getItem('token'); // Replace with your auth state
+
+    // If no token, redirect to login
+    if (!token) {
+        return <Navigate to="/auth/login" replace />;
+    }
+
+    else{return (
         <>
             <Header user={user} />
             <div className="main">
@@ -14,7 +23,7 @@ const HomeLayout = ({user}) => {
             </div>
             <Footer />
         </>
-     );
+    )}
 }
- 
+
 export default HomeLayout;
