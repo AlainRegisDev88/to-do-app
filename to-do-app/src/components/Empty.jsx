579471f72e56f062c4496f0b66766289866fbda9
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-25 20:23:21 +0200

    fix: separate backend from the frontend folder

--- to-do-app/src/components/Empty.jsx
+++
@@ -1,9 +0,0 @@
-const Empty = () => {
-    return ( 
-        <section>
-            
-        </section>
-     );
-}
- 
-export default Empty;
--- to-do-app/src/components/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-    return ( 
-        <div className="not-found-page">
-            <p>page not found</p>
-        </div>
-     );
-}
- 
-export default NotFound;
