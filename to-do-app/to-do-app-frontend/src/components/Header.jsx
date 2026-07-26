4f97d2a5955eedcd191fdb7ad71fb4c2d39f95db
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-26 11:55:24 +0200

    add username skeleton

--- to-do-app/to-do-app-frontend/src/components/Header.jsx
+++ to-do-app/to-do-app-frontend/src/components/Header.jsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
 import AvatarSkeleton from "./Skeleton/AvatarSkeleton";
+import UserNameSkeleton from "./Skeleton/UserNameSkeleton";
 
 const Header = ({ user, loadingUser}) => {
 
@@ -35,7 +36,7 @@ const Header = ({ user, loadingUser}) => {
             <div className="right-nav-bar">
                 <Link className="add-task-button" to='/new-task'>+ New Task</Link>
                 <Link to='/notifications'><FontAwesomeIcon className="bell-icon" icon={faBell} /></Link>
-                <div className="user-name">{formattedName}</div>
+                <div className="user-name">{loadingUser? <UserNameSkeleton /> :formattedName}</div>
                 <div className={`${loadingUser ? "": "avatar"}`}>{loadingUser ? <AvatarSkeleton /> :avatarInitial}</div>
             </div>
         </section>
