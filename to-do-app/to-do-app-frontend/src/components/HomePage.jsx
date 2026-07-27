2faa7c47f4eef3c4547c09c9010b0ad4b7410f41
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-27 07:40:00 +0200

    mend

--- to-do-app/to-do-app-frontend/src/components/HomePage.jsx
+++ to-do-app/to-do-app-frontend/src/components/HomePage.jsx
@@ -60,7 +60,7 @@ const HomePage = ({ user }) => {
     const completeTask = async (e) => {
         e.preventDefault()
         const currentId = e.currentTarget.dataset.id;
-        const response = await taskService.getTask(currentId);
+        const response = await taskService.retrieveTask(currentId);
     }
 
     const todayTasks = tasks.filter(task => task.title === "new task")
