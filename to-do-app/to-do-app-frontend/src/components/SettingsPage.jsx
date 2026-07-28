98c0a471b9c02432c42f3b8fefa37b00a7e3ccdd
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-28 21:35:45 +0200

    Add the update the account info form and open-close functionality

--- to-do-app/to-do-app-frontend/src/components/SettingsPage.jsx
+++ to-do-app/to-do-app-frontend/src/components/SettingsPage.jsx
@@ -1,4 +1,4 @@
-import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight, faClose } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './SettingsPage.css'
 import { useState } from "react";
@@ -9,94 +9,105 @@ const SettingsPage = ({ user }) => {
 
     const openEditProfileCard = (e) => {
         e.preventDefault()
+        const editUpdateForm = document.getElementById("edit-profile-card");
+            editUpdateForm.style.display = "flex";     
+    }
 
-        const editProfileCard = document.getElementById("edit-profile");
-
+    const closeEditProfileCard = () =>{
+        const editUpdateForm = document.getElementById("edit-profile-card");
+            editUpdateForm.style.display = "none";
     }
 
     const updateProfile = () => {
-        
+
     }
 
     return (
-        <section className="settings-page">
+        <>
+            <section className="settings-page">
 
-            <div id="edit-profile">
-                <form onSubmit={updateProfile}>
+                <div className="settings-header page-header">
+                    <p>Settings</p>
+                </div>
 
+                <div className="settings-cards">
+                    <div className="settings-card profile-card">
+                        <div className="profile-info">
+                            <div className="avatar">{user?.name?.charAt(0).toUpperCase()}</div>
+                            <div className="personal-info">
+                                <p className="name settings-card-heading">{user.name}</p>
+                                <p className="email">{user.email}</p>
+                            </div>
+                        </div>
+                        <div className="edit-profile">
+                            <button onClick={openEditProfileCard} className="edit-profile button-secondary">Edit Profile</button>
+                        </div>
+                    </div>
+
+                    <div className="settings-card preferences-card">
+                        <div className="preferences-heading settings-card-heading">
+                            <p>Preferences</p>
+                        </div>
+                        <div className="preference">
+                            <p className="settings-row">Dark mode</p>
+                            <input type="checkbox" name="dark-mode" />
+                        </div>
+
+                        <div className="preference">
+                            <p className="settings-row">Email reminders</p>
+                            <input type="checkbox" name="email-messages" />
+                        </div>
+                    </div>
+
+                    <div className="settings-card account-settings">
+                        <div className="settings-card-heading">
+                            <p>Account</p>
+                        </div>
+                        <div className="setting change-password">
+                            <p className="settings-row">Change password</p>
+                            <FontAwesomeIcon className="chevron-icon" icon={faChevronRight} />
+                        </div>
+                        <div className="setting delete-account">
+                            <p className="settings-row delete-row">Delete Account</p>
+                            <FontAwesomeIcon className="chevron-icon" icon={faChevronRight} />
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <div id="edit-profile-card">
+                <form className="edit-profile-form" onSubmit={updateProfile}>
+                    <div onClick={closeEditProfileCard} className="close-button" id="close-button"><FontAwesomeIcon icon={faClose} /></div>
+                    <div className="form-title">
+                        <p>Update your profile</p>
+                    </div>
                     <div className="form-item">
-                        <label htmlFor="name">Change your username: </label>
+                        <label htmlFor="name">Enter new username </label>
                         <input
                             className="input-box"
                             type="text"
                             name="new-username"
                             value={newUsername}
-                            onChange={(e)=> setNewUsername(e.target.value)}
+                            onChange={(e) => setNewUsername(e.target.value)}
                         />
                     </div>
 
                     <div className="form-item">
-                        <label htmlFor="email">Change your email: </label>
+                        <label htmlFor="email">Enter new email </label>
                         <input
                             className="input-box"
                             type="email"
                             name="new-email"
                             value={newEmail}
-                            onChange={(e)=> setNewEmail(e.target.value)}
+                            onChange={(e) => setNewEmail(e.target.value)}
                         />
                     </div>
-
+                    <div className="form-item">
+                        <button className="submit-button button-secondary">Update profile</button>
+                    </div>
                 </form>
             </div>
-
-            <div className="settings-header page-header">
-                <p>Settings</p>
-            </div>
-
-            <div className="settings-cards">
-                <div className="settings-card profile-card">
-                    <div className="profile-info">
-                        <div className="avatar">{user?.name?.charAt(0).toUpperCase()}</div>
-                        <div className="personal-info">
-                            <p className="name settings-card-heading">{user.name}</p>
-                            <p className="email">{user.email}</p>
-                        </div>
-                    </div>
-                    <div className="edit-profile">
-                        <button onClick={openEditProfileCard} className="edit-profile button-secondary">Edit Profile</button>
-                    </div>
-                </div>
-
-                <div className="settings-card preferences-card">
-                    <div className="preferences-heading settings-card-heading">
-                        <p>Preferences</p>
-                    </div>
-                    <div className="preference">
-                        <p className="settings-row">Dark mode</p>
-                        <input type="checkbox" name="dark-mode" />
-                    </div>
-
-                    <div className="preference">
-                        <p className="settings-row">Email reminders</p>
-                        <input type="checkbox" name="email-messages" />
-                    </div>
-                </div>
-
-                <div className="settings-card account-settings">
-                    <div className="settings-card-heading">
-                        <p>Account</p>
-                    </div>
-                    <div className="setting change-password">
-                        <p className="settings-row">Change password</p>
-                        <FontAwesomeIcon className="chevron-icon" icon={faChevronRight} />
-                    </div>
-                    <div className="setting delete-account">
-                        <p className="settings-row delete-row">Delete Account</p>
-                        <FontAwesomeIcon className="chevron-icon" icon={faChevronRight} />
-                    </div>
-                </div>
-            </div>
-        </section>
+        </>
     );
 }
 
