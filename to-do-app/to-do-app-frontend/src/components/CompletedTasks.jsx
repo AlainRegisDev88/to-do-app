4335ee92b8bc03c49983a595463f8366bab3124e
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-28 16:26:16 +0200

    initiate completed tasks rendering

---
+++ to-do-app/to-do-app-frontend/src/components/CompletedTasks.jsx
@@ -0,0 +1,13 @@
+import './CompletedTasks.css'
+
+const CompletedTasks = () => {
+    return ( 
+        <section className="completed-tasks">
+            <div className="completed-tasks-header">
+                Completed tasks
+            </div>
+        </section>
+     );
+}
+ 
+export default CompletedTasks;
