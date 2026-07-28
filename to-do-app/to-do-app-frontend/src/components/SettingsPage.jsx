86fe7eee5ed85d79cfe723d24ea10cea87267139
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-28 20:40:46 +0200

    Add the edit account form

--- to-do-app/to-do-app-frontend/src/components/SettingsPage.jsx
+++ to-do-app/to-do-app-frontend/src/components/SettingsPage.jsx
@@ -1,10 +1,54 @@
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './SettingsPage.css'
+import { useState } from "react";
 
-const SettingsPage = () => {
-    return ( 
+const SettingsPage = ({ user }) => {
+    const [newUsername, setNewUsername] = useState("")
+    const [newEmail, setNewEmail] = useState("")
+
+    const openEditProfileCard = (e) => {
+        e.preventDefault()
+
+        const editProfileCard = document.getElementById("edit-profile");
+
+    }
+
+    const updateProfile = () => {
+        
+    }
+
+    return (
         <section className="settings-page">
+
+            <div id="edit-profile">
+                <form onSubmit={updateProfile}>
+
+                    <div className="form-item">
+                        <label htmlFor="name">Change your username: </label>
+                        <input
+                            className="input-box"
+                            type="text"
+                            name="new-username"
+                            value={newUsername}
+                            onChange={(e)=> setNewUsername(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="form-item">
+                        <label htmlFor="email">Change your email: </label>
+                        <input
+                            className="input-box"
+                            type="email"
+                            name="new-email"
+                            value={newEmail}
+                            onChange={(e)=> setNewEmail(e.target.value)}
+                        />
+                    </div>
+
+                </form>
+            </div>
+
             <div className="settings-header page-header">
                 <p>Settings</p>
             </div>
@@ -12,14 +56,14 @@ const SettingsPage = () => {
             <div className="settings-cards">
                 <div className="settings-card profile-card">
                     <div className="profile-info">
-                        <div className="avatar">RU</div>
+                        <div className="avatar">{user?.name?.charAt(0).toUpperCase()}</div>
                         <div className="personal-info">
-                            <p className="name settings-card-heading">Regis Udahemuka</p>
-                            <p className="email">regis@gmail.com</p>
+                            <p className="name settings-card-heading">{user.name}</p>
+                            <p className="email">{user.email}</p>
                         </div>
                     </div>
                     <div className="edit-profile">
-                        <button className="edit-profile button-secondary">Edit Profile</button>
+                        <button onClick={openEditProfileCard} className="edit-profile button-secondary">Edit Profile</button>
                     </div>
                 </div>
 
@@ -29,12 +73,12 @@ const SettingsPage = () => {
                     </div>
                     <div className="preference">
                         <p className="settings-row">Dark mode</p>
-                        <input type="checkbox" name="dark-mode"/>
+                        <input type="checkbox" name="dark-mode" />
                     </div>
 
                     <div className="preference">
                         <p className="settings-row">Email reminders</p>
-                        <input type="checkbox" name="email-messages"/>
+                        <input type="checkbox" name="email-messages" />
                     </div>
                 </div>
 
@@ -53,7 +97,7 @@ const SettingsPage = () => {
                 </div>
             </div>
         </section>
-     );
+    );
 }
- 
+
 export default SettingsPage;
